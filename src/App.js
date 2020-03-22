6ae1693ce50033170adc56dbda89c1aaf8beb61e
“bikale” <bikaleg@gmail.com>
2020-03-21 22:31:43 -0500

    build components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
-import Todos from './components/Todos';
+import * as uuid from 'uuid';
 
 import './App.css';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import Header from './components/layout/Header';
 
 class App extends Component {
   state = {
     todos: [
-      { id: 1, title: 'Take out the trash', completed: false },
-      { id: 2, title: 'Finish Homework', completed: false },
-      { id: 3, title: 'Learn GraphQL', completed: false }
+      { id: uuid.v4(), title: 'Take out the trash', completed: false },
+      { id: uuid.v4(), title: 'Finish Homework', completed: false },
+      { id: uuid.v4(), title: 'Learn GraphQL', completed: true }
     ]
   };
-  render() {
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+  //Delete todo list
+  delTodo = id => {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id)
+    });
+  };
 
+  //Add Todo
+  addTodo = title => {
+    const newTodo = { id: uuid.v4(), title, completed: false };
+
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
+  
+  };
+
+  render() {
     return (
       <div className="App">
-       
-        <Todos todos={this.state.todos} />
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
